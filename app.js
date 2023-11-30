6e41e6912d92634adfecfd103bc38ead043b43d3
Serge Gavrilov <sergegavrilov01@gmail.com>
2023-11-30 09:00:19 +0000

    portfolio

---
+++ app.js
@@ -0,0 +1,22 @@
+function sectionOne() {
+    var scrollDiv = document.getElementById("sec_one").offsetTop;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
+
+function sectionTwo() {
+    var scrollDiv = document.getElementById("sec_two").offsetTop;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
+
+function sectionThree() {
+    var scrollDiv = document.getElementById("sec_three").offsetTop;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
+
+function sectionFour() {
+    var scrollDiv = document.getElementById("sec_four").offsetTop;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
+
+
+
